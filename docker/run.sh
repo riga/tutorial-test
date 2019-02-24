f615ae7000f605af63c2d8b3c9a9bf4afdf28fb8
Marcel R <marcelrieger@me.com>
2019-02-24 22:09:27 +0100

    Improve docker setup.

--- docker/run.sh
+++ docker/run.sh
@@ -11,29 +11,22 @@ action() {
     local repo_dir="$( cd "$( dirname "$this_dir" )" && pwd )"
 
     # some configs
-    [ -z "$DOCKER_PORT" ] && DOCKER_PORT="8888"
     [ -z "$DOCKER_ROOT" ] && DOCKER_ROOT="0"
+    [ -z "$DOCKER_PORT" ] && DOCKER_PORT="8888"
 
-    # run the
-    if [ "$DOCKER_ROOT" != "1" ]; then
-        docker run \
-            --rm \
-            -ti \
-            -w /tutorial \
-            -v "$repo_dir":/tutorial \
-            -e "NB_PORT=$DOCKER_PORT" \
-            -p "$DOCKER_PORT":"$DOCKER_PORT" \
-            -u $(id -u):$(id -g) \
-            riga/iml2019lbn $@
-    else
-        docker run \
-            --rm \
-            -ti \
-            -w /tutorial \
-            -v "$repo_dir":/tutorial \
-            -e "NB_PORT=$DOCKER_PORT" \
-            -p "$DOCKER_PORT":"$DOCKER_PORT" \
-            riga/iml2019lbn $@
-    fi
+    # user option for docker run, depends on whether to run as root or not
+    local user_opt="-u $(id -u):$(id -g)"
+    [ "$DOCKER_ROOT" = "1" ] && user_opt=""
+
+    # run the container
+    docker run \
+        --rm \
+        -ti \
+        -w /tutorial \
+        -v "$repo_dir":/tutorial \
+        -e "NB_PORT=$DOCKER_PORT" \
+        -p $DOCKER_PORT:$DOCKER_PORT \
+        $user_opt \
+        riga/iml2019lbn $@
 }
 action "$@"
--- tutorial.py
+++ tutorial.py
@@ -6,10 +6,14 @@ Helpful utilities for the LBN tutorial at the IML workshop 2019.
 
 import os
 import sys
+import functools
 
 import numpy as np
 import six
 
+
+# print function with auto-flush
+print_ = functools.partial(six.print_, flush=True)
 
 # define directories and urls
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,18 +23,12 @@ eos_url = "https://cernbox.cern.ch/index.php/s/xDYiSmbleT3rip4/download?path=%2F
 
 # check if we have access to /eos or not
 has_eos = os.access(eos_dir, os.R_OK)
+print_("eos access: {}".format("✔︎" if has_eos else "✗"), flush=True)
 
-# if eos is accessible, amend the path to find shared software
+# if eos is accessible, amend sys.path to find shared software
 # otherwise, software must be installed manually (or via requirements.txt on binder)
 if has_eos:
     sys.path.insert(0, os.path.join(eos_dir, "software/lib/python2.7/site-packages"))
-six.print_("eos access: {}".format("✔︎" if has_eos else "✗"))
-
-    
-# file download helper
-def download(src, dst, bar=None):
-    import wget
-    return wget.download(src, out=dst, bar=bar)
 
 
 # data loading helper
@@ -40,7 +38,6 @@ def load_lbn_data(level="low", sorting="gen", kind="train"):
     and *kind* ("train" or "test"). The return value is dictionary-like object with two keys,
     "labels" and "features", which point to plain numpy arrays.
     """
-    # check arguments
     levels = ("low", "high", "mixed")
     if level not in levels:
         raise ValueError("unknown dataset level '{}', must be one of {}".format(
@@ -63,11 +60,16 @@ def load_lbn_data(level="low", sorting="gen", kind="train"):
             os.mkdir(data_dir)
         file_path = os.path.join(data_dir, file_name)
         if not os.path.exists(file_path):
-            six.print_("downloading {} from CERNBox".format(file_name), flush=True)
+            print_("downloading {} from CERNBox".format(file_name), flush=True)
             download(eos_url.format(file_name), file_path)
     else:
         file_path = os.path.join(eos_dir, "data", file_name)
-    
+
     # open and return the numpy file object
     return np.load(file_path)
-        
+
+
+# file download helper
+def download(src, dst, bar=None):
+    import wget
+    return wget.download(src, out=dst, bar=bar)
